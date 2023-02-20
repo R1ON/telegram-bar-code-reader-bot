293f259778de4112ff46ebc299797e7071c3a3a4
Roman Maksimov <get.my.email.name@yandex.ru>
2023-02-20 13:31:16 +0600

    fix worker

--- src/seed/worker.ts
+++ src/seed/worker.ts
@@ -2,7 +2,10 @@ import { Worker } from 'worker_threads';
 
 function createWorker(csvFiles: string[]) {
   return new Promise<void>((resolve, reject) => {
-    const worker = new Worker(__filename, { workerData: csvFiles });
+    const worker = new Worker(__filename, {
+      workerData: csvFiles,
+      execArgv: ["--require", "ts-node/register"],
+    });
 
     worker.on('message', (message) => {
       console.log(message);
